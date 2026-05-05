09de92b3d0f5ae526cab1552f772fa54e4dfefab
Manoj <manoj12k6@gmail.com>
2026-05-06 00:00:33 +0530

    production ready

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -26,7 +26,7 @@ const steps = [
   {
     num: 'Step 03', title: 'Installation',
     desc: 'Certified technicians install the complete system with quality assurance.',
-    img: 'https://img.sanishtech.com/u/c3ad705c066a2634e55e35d98f95f426.webp',
+    img: './installation.webp',
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="28" height="28">
         <path d="M22.7 19l-9.1-9.1c.9-2.3.4-5-1.5-6.9-2-2-5-2.4-7.4-1.3L9 6 6 9 1.6 4.7C.4 7.1.9 10.1 2.9 12.1c1.9 1.9 4.6 2.4 6.9 1.5l9.1 9.1c.4.4 1 .4 1.4 0l2.3-2.3c.5-.4.5-1.1.1-1.4z"/>
